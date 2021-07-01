6b45f360ec814fef19c46b03480bbb4ab0dfbc7d
Josephine <josephine.bartholoma@gmail.com>
2021-07-01 14:45:22 -0400

    Revert "feat: created models"

--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,14 @@
 //this is the access point for all things database related!
 
-const db = require("../db");
-const ArtPieces = require("./ArtPieces");
-const Voronoi = require("./Voronoi");
-const User = require("./models/User");
+const db = require('./db')
+
+const User = require('./models/User')
 
 //associations could go here!
-ArtPieces.belongsTo(Voronoi, { foreignKey: VoronoiId  });
-//Voronoi.belongsTo(ArtPieces, {through: VoronoiElements});
-Voronoi.hasMany(ArtPieces);
 
 module.exports = {
   db,
   models: {
-    ArtPieces,
-    Voronoi,
+    User,
   },
-};
+}
